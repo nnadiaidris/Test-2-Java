1aeee6aa10fa967f612f8338c3bd61992a695b70
nnadiaidris <n.nadiaidris@gmail.com>
2015-10-10 14:48:00 +0800

    new commit

---
+++ src/T.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.util.*;
+import static java.lang.System.out;
+
+public class T {
+
+    public static void main(String args[]) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
+    }
+
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        out.printf("Display name: %s\n", netint.getDisplayName());
+        out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        Collections.list(inetAddresses).stream().forEach((TAddress) -> {
+            Object[] inetAddress;
+            out.printf("InetAddress: %s\n", inetAddress);
+        });
+        out.printf("\n");
+     }
+}  
--- src/test/pkg2/App.java
+++ src/test/pkg2/App.java
@@ -18,26 +18,30 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class App{
+    private static Object ipv6;
 	
    public static void main(String[] args){
 		
 	InetAddress ip;
 	try {
-			
+            	
 		ip = InetAddress.getLocalHost();
 		System.out.println("Current IP address : " + ip.getHostAddress());
-		
+               
 		NetworkInterface network = NetworkInterface.getByInetAddress(ip);
 			
 		byte[] mac = network.getHardwareAddress();
-			
+                
 		System.out.print("Current MAC address : ");
+                
 			
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < mac.length; i++) {
-			sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
-		}
+			sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                 
+                }
 		System.out.println(sb.toString());
+         
 			
 	} catch (UnknownHostException | SocketException e) {
 	}
