88dc4b16fbde7b8854924e9bbc6d659abd58823d
nnadiaidris <n.nadiaidris@gmail.com>
2015-10-10 13:44:14 +0800

    new commit

---
+++ src/test/pkg2/addr.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.pkg2;
+
+/**
+ *
+ * @author Nurul.Nadia.Idris
+ */
+class addr {
+
+    static boolean getHostAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
